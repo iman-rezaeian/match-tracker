eb3b2487d4f3e2afbf4084d7e7b03952fffe5f13
Rezaeian, Iman <rezaeian.iman@gmail.com>
2026-05-27 11:19:14 -0400

    feat: configurable half length (default 25min); scorebug period+minute derived from video playback time (TV-broadcast style)

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *
  * Bump CACHE_VERSION whenever the shell or icons change so old caches purge.
  */
-const CACHE_VERSION = 'stompers-v11';
+const CACHE_VERSION = 'stompers-v12';
 const SHELL = [
   './',
   './index.html',
