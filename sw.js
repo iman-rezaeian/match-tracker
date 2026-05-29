f810cd1f427b4bb0b6057fc234d95a590bc03d9a
Rezaeian, Iman <rezaeian.iman@gmail.com>
2026-05-29 14:33:57 -0400

    scorebug: concave corner fillets fuse row 2 into row 1 as a single broadcast-graphic shape

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *
  * Bump CACHE_VERSION whenever the shell or icons change so old caches purge.
  */
-const CACHE_VERSION = 'stompers-v42';
+const CACHE_VERSION = 'stompers-v43';
 const SHELL = [
   './',
   './index.html',
