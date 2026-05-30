30959e18660248792ae540167a15495b9c599a92
Rezaeian, Iman <rezaeian.iman@gmail.com>
2026-05-29 23:55:06 -0400

    fix: hide YouTube player chrome (controls/branding) in embed (v51)

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *
  * Bump CACHE_VERSION whenever the shell or icons change so old caches purge.
  */
-const CACHE_VERSION = 'stompers-v50';
+const CACHE_VERSION = 'stompers-v51';
 const SHELL = [
   './',
   './index.html',
