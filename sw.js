192faf330ed542605dd2c0b9937e98388d8dd8e8
Rezaeian, Iman <rezaeian.iman@gmail.com>
2026-05-30 00:19:02 -0400

    chore: cleanup dead HLS proxy code (v54)

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  *
  * Bump CACHE_VERSION whenever the shell or icons change so old caches purge.
  */
-const CACHE_VERSION = 'stompers-v53';
+const CACHE_VERSION = 'stompers-v54';
 const SHELL = [
   './',
   './index.html',
